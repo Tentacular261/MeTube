3ee066a8421194673f3b4d6e34e19a1911963069
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-02-19 14:21:00 -0500

    Playing with css to get centered posts

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html>
     <head>
         <title>Hello World</title>
+        <link rel="stylesheet" href="css/browsing.css"
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
         <p>sign in</p>
         <p><a href="user/register.php">Register</a></p>
+        <div class="items">
+            <?php for ($i=0;$i<25;$i++) { ?>
+                <img class="item" src="media/heh.png" alt="heh" height="64" width="64">
+            <?php } ?>
+        </div>
     </body>
 </html>
