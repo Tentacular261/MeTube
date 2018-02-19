d13b28cafb736702caf78475d8d755d40571abdb
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-02-19 11:33:57 -0500

    Fixing indentation in index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Hello World</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?>
-    <p>sign in</p>
-    <p><a href="user/register.php">Register</a></p>
-  </body>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+        <p>sign in</p>
+        <p><a href="user/register.php">Register</a></p>
+    </body>
 </html>
