77f80fdd7888e43c4eedda881cce31271a360f0e
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-03-06 16:09:37 -0500

    Added username to the post page

--- post.php
+++ post.php
@@ -48,6 +48,7 @@ if ($result->num_rows != 1) {
         <div class="main_image">
             <img class="main" src="media/<?php echo $file; ?>">
             <div class="title"><?php echo $title; ?></div><br>
+            <div class="user"><?php echo $uploader; ?></div><br>
             <div class="description"><?php echo $description; ?></div>
         </div>
         <p><a href="index.php">Return to index</a></p>
