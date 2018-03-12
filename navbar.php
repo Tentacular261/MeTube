0fdac3d8a59e513265a681da98254aed70f54c18
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-03-11 23:29:35 -0400

    Dynamic changing of navbar if logged in

--- navbar.php
+++ navbar.php
@@ -1,13 +1,119 @@
 
 <div class="topnav" id="myTopnav">
-    <a class="user" href="#home" top="true">
+    <a class="user" href="user.php" top="true"> <!--TODO: create the user account control page -->
         <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-teacher-312a499a08079a12-512x512.png"/>
-        <!-- CHANGE FOR UNIQUE USER AVATAR -->
-        <span>Welcome User</span>
+        <!-- TODO: CHANGE FOR UNIQUE USER AVATAR -->
+        <span>Welcome <?php echo isset($_SESSION['username']) ? $_SESSION['username'] : "Guest" ?></span>
     </a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a class="right" href="#about">Testing</a>
+    <?php
+    if (isset($_SESSION['username'])) {
+    ?>
+        <a href="index.php">Home</a>
+        <a href="upload.php">Upload</a>
+        <!-- TODO: make log out work -->
+        <a class="right" href="index.php">Log Out</a>
+    <?php }
+    else { ?>
+        <a class="right" href="javascript:void(0);"
+        onclick="document.getElementById('register').style.display='block'">
+           Register
+        </a>
+        <a class="right" href="javascript:void(0);"
+        onclick="document.getElementById('login').style.display='block'">
+           Log In
+        </a>
+
+        <!-- TODO: Go through and integrate these dialogs -->
+        <!-- Login Popup -->
+        <div id="login" class="modal">
+
+            <form class="modal-content animate" action="">
+            <!-- CHANGE TO DB LOGIN -->
+
+                <div class="container">
+                    <span onclick="document.getElementById('login').style.display='none'"
+                    class="close" title="Close">&times;</span>
+                    <h1>Login</h1>
+                    <hr>
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username"
+                           name="uname" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password"
+                           name="psw" required>
+                    <br>
+
+                    <button type="button" onclick="document.getElementById('login').style.display='none'"
+                            class="cancelbtn">
+                        Cancel
+                    </button>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+
+        <script>
+            // Get the modal
+            var modal = document.getElementById('login');
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+
+        <!-- Register Popup -->
+        <div id="register" class="modal">
+
+            <form class="modal-content animate" action="">
+                <!-- CHANGE TO DB NEW USER -->
+                <div class="container">
+                    <span onclick="document.getElementById('register').style.display='none'"
+                          class="close" title="Close">&times;</span>
+                    <h1>Register</h1>
+                    <p>Please fill in this form to create an account.</p>
+                    <hr>
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username"
+                        name="username" required />
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password"
+                        name="psw" required>
+
+                    <label for="psw-repeat"><b>Repeat Password</b></label>
+                    <input type="password" placeholder="Repeat Password"
+                        name="psw-repeat" required>
+
+                    <p>By creating an account you agree to absolutely no privacy or exclusivity of content.</p>
+
+                    <div class="clearfix">
+                    <button type="button" onclick="document.getElementById('register').style.display='none'"
+                    class="cancelbtn">
+                    Cancel
+                    </button>
+                    <button type="submit" class="registerbtn">Register</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <script>
+            // Get the modal
+            var modal = document.getElementById('register');
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+
+    <?php } ?>
     <a href="javascript:void(0);" class="icon" top="true" onclick="myFunction()">&#9776;</a>
 </div>
 <script>
