1e91955febd84a56e30a4614f9d7914fb03df805
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-03-11 21:36:39 -0400

    Adding the user avatar to the navbar

--- navbar.php
+++ navbar.php
@@ -1,10 +1,14 @@
 
 <div class="topnav" id="myTopnav">
-    <a href="#home" class="active">Home</a>
+    <a class="user" href="#home" top="true">
+        <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-teacher-312a499a08079a12-512x512.png"/>
+        <!-- CHANGE FOR UNIQUE USER AVATAR -->
+        <span>Welcome User</span>
+    </a>
     <a href="#news">News</a>
     <a href="#contact">Contact</a>
     <a class="right" href="#about">Testing</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    <a href="javascript:void(0);" class="icon" top="true" onclick="myFunction()">&#9776;</a>
 </div>
 <script>
 function myFunction() {
