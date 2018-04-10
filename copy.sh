26353c5ad59e3e90fcb78c773615149d0e148134
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-04-10 16:00:15 -0400

    Updating utility scripts

--- copy.sh
+++ copy.sh
@@ -1,2 +1,4 @@
 cp -rf /home/wzsulli/public_html/metube/media ./media
+cp -rf /home/ssweetm/public_html/metube/media ./media
+cp -rf /home/micah6/public_html/MeTube/media ./media
 chmod 755 media media/*
--- fixperms.sh
+++ fixperms.sh
@@ -6,3 +6,5 @@ find . -type f \( -name \*.php -or -name .gitignore \) -exec chmod -v 600 '{}' +
 
 # make css and js source and .htaccess publicly readable
 find . -type f \( -name \*.css -or -name \*.js -or -name .htaccess \) -exec chmod -v 604 '{}' + | grep -v "retained"
+
+chmod -v 755 media media/* | grep -v "retained"
