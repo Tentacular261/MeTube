da40db35fa35fc20f48fee9332baec8b191632c7
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-04-10 16:13:56 -0400

    fixing copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,16 @@
-cp -rf /home/wzsulli/public_html/metube/media ./media
-cp -rf /home/ssweetm/public_html/metube/media ./media
-cp -rf /home/micah6/public_html/MeTube/media ./media
+if [ $(echo $USER) !=  "wzsulli" ]
+then
+    cp -rf /home/wzsulli/public_html/metube/media ./media
+fi
+
+if [ $(echo $USER) !=  "ssweetm" ]
+then
+    cp -rf /home/ssweetm/public_html/metube/media ./media
+fi
+
+if [ $(echo $USER) !=  "micah6" ]
+then
+    cp -rf /home/micah6/public_html/MeTube/media ./media
+fi
+
 chmod 755 media media/*
