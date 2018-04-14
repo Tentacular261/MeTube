b4c735acb52d41faf17168279b040e9c864e3177
Sadie Sweetman <ssweetm@g.clemson.edu>
2018-04-13 20:10:02 -0400

    deleted zack code commenting

--- upload.php
+++ upload.php
@@ -202,69 +202,6 @@
 			</div>
 		</div>
 
-		<div class="userContent">
-			<div class="userContentRow">
-				<div class="userContentCol">
-					<div class="main-search">
-						<form action="/action_page.php">   <!-- TODO: CHANGE TO GIVE RESULTS -->
-							<input type="text" placeholder="Search your media..." name="search">
-							<select id="category" name="category" />
-								<option value="" disabled selected>Filter by Category</option>
-								<option value="entertainment">Entertainment</option>
-								<option value="food">Food</option>
-								<option value="funny">Funny</option>
-								<option value="gaming">Gaming</option>
-								<option value="news">News & Politics</option>
-								<option value="people">People</option>
-								<option value="pets">Pets & Animals</option>
-								<option value="science">Science & Tech</option>
-								<option value="sports">Sports</option>
-								<option value="travel">Travel & Outdoors</option> </select>
-							<select id="lists" name="lists" />
-								<option value="" disabled selected>Filter by List</option>
-								<option value="favorites">Favorites </option>
-								<option value="haha">Haha (Example) </option>
-								<option value="rip">RIP (Example) </option> </select>
-
-							<!-- TODO: SEARCH BY FILE TYPE -->
-							File Type: 
-							<input type="checkbox" name="file" value="image" checked>Images
-							<input type="checkbox" name="file" value="video" checked>Video
-							<input type="checkbox" name="file" value="audio" checked>Audio <br/>
-							Rating:
-							<input type="checkbox" name="rating" value="1">1
-							<input type="checkbox" name="rating" value="2">2
-							<input type="checkbox" name="rating" value="3">3 
-							<input type="checkbox" name="rating" value="4">4 
-							<input type="checkbox" name="rating" value="5">5 </br>
-							Privacy:
-							<input type="checkbox" name="privacy" value="public" checked>Public               
-							<input type="checkbox" name="privacy" value="private" checked>Private
-							<input type="checkbox" name="privacy" value="contacts" checked>Friends <br/>
-
-							Uploaded: </br>
-							Between<input type="date" name="date">
-							And<input type="date" name="date">
-
-							<div class="userContentRow">
-								<div class="userContentCol">
-									<!-- TODO: NEW ACTION FOR SEARCH-->
-									<button type="submit" name="search">Search</button>
-								</div>
-								<div class="userContentCol">
-									<!-- TODO: NEW ACTION FOR RESET -->
-									<button type="submit" name="reset">Reset</button>
-								</div>
-							</div>
-                  </form>
-               </div>
-				</div>
-
-				<div class="userContentCol">
-				</div>
-			</div>
-		</div>
-
 		<!-- Footer Content -->
       <div class="footer">
             <h6><b>CPSC 4620-001 Spring 2018</b><br><i>Micah Johnson, Zackary Sullivan,  Sadie Sweetman</i></h6>
