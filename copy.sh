e8e7788ef2aa6ed19b968713a573e8410dcd79fa
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-04-14 15:38:40 -0400

    Fixing copy script

--- copy.sh
+++ copy.sh
@@ -2,21 +2,21 @@ if [ $(echo $USER) !=  "wzsulli" ]
 then
     echo "copy wzsulli"
     cp -rf /home/wzsulli/public_html/metube/media .
-    cp -rf /home/wzsulli/public_html/metube/user/profile_pictures .
+    cp -rf /home/wzsulli/public_html/metube/user/profile_pictures user/
 fi
 
 if [ $(echo $USER) !=  "ssweetm" ]
 then
     echo "copy ssweetm"
     cp -rf /home/ssweetm/public_html/metube/media .
-    cp -rf /home/ssweetm/public_html/metube/user/profile_pictures .
+    cp -rf /home/ssweetm/public_html/metube/user/profile_pictures user/
 fi
 
 if [ $(echo $USER) !=  "micah6" ]
 then
     echo "copy micah6"
     cp -rf /home/micah6/public_html/MeTube/media .
-    cp -rf /home/micah6/public_html/MeTube/user/profile_pictures .
+    cp -rf /home/micah6/public_html/MeTube/user/profile_pictures user/
 fi
 
 chmod 755 media media/*
