cd530e2e47e03ffa7007a3c073d99d3ff1069eb7
W. Zackary Sullivan <wzsulli@clemson.edu>
2018-04-16 09:33:34 -0400

    getting rid of errors in the ready script

--- ready.sh
+++ ready.sh
@@ -1,7 +1,7 @@
 chmod 700 copy.sh fixperms.sh
 
-./copy.sh 2>&1 >/dev/null
+./copy.sh 2>/dev/null 1>/dev/null
 
-./fixperms.sh 2>&1 >/dev/null
+./fixperms.sh 2>/dev/null 1>/dev/null
 
 echo "I'm good to go!"
